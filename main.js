de6b873f4a23e1361e79f8fb75e9cf9b88dbfce5
samarasandrin-alt <samara.sandrin@escola.pr.gov.br>
2026-05-25 13:44:07 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-<script src="main.js"></script>
 const botoes = document.querySelectorAll(".botao");
-console.log(botoes);
-botoes[i].onclick = function () {};
-botoes[i].onclick = function () {
-    botoes[i].classList.add("ativo");
-  };
-  botoes[i].onclick = function () {
-    botoes[i].classList.add("ativo");
-  };
-  for (let i = 0; i < botoes.length; i++) {
+const textos = document.querySelectorAll(".aba-conteudo");
+for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
+      for (let j = 0; j < botoes.length; j++) {
+        botoes[j].classList.remove("ativo");
+        textos[j].classList.remove("ativo");
+      }
+  
       botoes[i].classList.add("ativo");
+      textos[i].classList.add("ativo");
     };
   }
-  for (let j = 0; j < botoes.length; j++) {
-    botoes[j].classList.remove("ativo");
-  }
+
+  const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
